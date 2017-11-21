184689a4915d74025bd5de802673eab0fa6d958f
Ben Van Citters <van.citters.b@gmail.com>
2017-11-21 01:15:49 -0800

    adding a crude gaze system

---
+++ Assets/Scripts/GazeReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeReceiver : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ReceiveGaze()
+    {
+        Debug.Log("dude");
+    }
+}
---
+++ Assets/Scripts/GazeTrigger.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeTrigger : MonoBehaviour
+{
+    public float tickDownTime = 5.0f;
+
+
+    private Camera myCam;
+    private Transform hmdHead = null;
+
+    void findHead()
+    {
+        if (hmdHead == null)
+        {
+            SteamVR_TrackedObject[] trackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
+            foreach (SteamVR_TrackedObject tracked in trackedObjects)
+            {
+                if (tracked.index == SteamVR_TrackedObject.EIndex.Hmd)
+                {
+                    hmdHead = tracked.transform;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Use this for initialization
+	void Start ()
+    {
+        myCam = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+        //Ray ray = myCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        //RaycastHit hit;
+        //Physics.Raycast(ray,)
+        //Vector3 start = hmdHead.position;
+        //Vector3 end = (hmdHead.position + hmdHead.forward * 100);
+        //Ray ray = new Ray(start, end);
+        //Physics.Raycast(ray);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            GazeReceiver gr = hit.transform.gameObject.GetComponent<GazeReceiver>();
+            if(gr)
+            {
+                gr.ReceiveGaze();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, (transform.position + transform.forward * 100));
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            Gizmos.DrawCube(hit.point, new Vector3(0.1f, 0.1f, 0.1f));
+        }
+    }
+}
