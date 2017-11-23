5b61fde034b6608a4a6acc1e663de8f237ee900b
Ben Van Citters <van.citters.b@gmail.com>
2017-11-22 20:53:06 -0800

    Adding a flower gaze reciever and adding a flower collider

---
+++ Assets/FlowerGazeReciever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGazeReciever : GazeReceiver {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void GazeEntryTriggerOnce(RaycastHit hit)
+    {
+        Debug.Log("FlowerGazeReciever GazeEntryTriggerOnce");
+    }
+
+    protected override void GazeDelayTriggerOnce(RaycastHit hit)
+    {
+        Debug.Log("FlowerGazeReciever GazeDelayTriggerOnce");
+    }
+
+    protected override void GazeUpdate(RaycastHit hit)
+    {
+        Debug.Log("FlowerGazeReciever GazeUpdate");
+    }
+}
