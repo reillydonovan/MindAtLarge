bb811cc5428a013b0c0529db4cce28bb0c78f409
Ben Van Citters <van.citters.b@gmail.com>
2017-11-27 23:46:18 -0800

    Adding Brandon's unity transmission code

---
+++ Assets/Scripts/UnitySocketSend.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using System.IO;
+
+using System.Net.Sockets;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class UnitySocketSend : MonoBehaviour
+{
+
+
+
+    private String host = "192.168.1.112";
+
+    private Int32 port = 9999;
+
+    TcpClient socket;
+
+    public NetworkStream stream;
+
+    StreamWriter writer;
+
+    //StreamReader reader;
+
+
+
+    void Start()
+    {
+
+
+
+        try
+        {
+
+            socket = new TcpClient(host, port);
+
+            stream = socket.GetStream();
+
+            writer = new StreamWriter(stream);
+
+            //reader = new StreamReader(stream);
+
+            writer.Write("monteverdi/0.8");
+
+            writer.Flush();
+
+        }
+
+        catch (Exception e)
+        {
+
+            Debug.Log("Socket error:" + e);
+
+        }
+
+    }
+
+
+
+    void Update()
+    {
+
+
+
+    }
+
+}
