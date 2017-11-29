6ee963723e6643e96c871e67ce81d6bd42dce4ae
Ben Van Citters <van.citters.b@gmail.com>
2017-11-28 21:50:06 -0800

    remove extra newlines in socketsend

--- Assets/Scripts/UnitySocketSend.cs
+++ Assets/Scripts/UnitySocketSend.cs
@@ -1,74 +1,36 @@
 ﻿using System;
-
 using System.IO;
-
 using System.Net.Sockets;
-
 using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
-
-
 
 public class UnitySocketSend : MonoBehaviour
 {
-
-
-
-    private String host = "192.168.1.112";
-
+    private String host = "10.0.0.9";
     private Int32 port = 9999;
-
     TcpClient socket;
-
     public NetworkStream stream;
-
     StreamWriter writer;
-
     //StreamReader reader;
-
-
 
     void Start()
     {
-
-
-
         try
         {
-
             socket = new TcpClient(host, port);
-
             stream = socket.GetStream();
-
             writer = new StreamWriter(stream);
-
             //reader = new StreamReader(stream);
-
             writer.Write("monteverdi/0.8");
-
             writer.Flush();
-
         }
-
         catch (Exception e)
         {
-
             Debug.Log("Socket error:" + e);
-
         }
-
     }
-
-
 
     void Update()
-    {
-
-
-
-    }
-
+    {  }
 }
