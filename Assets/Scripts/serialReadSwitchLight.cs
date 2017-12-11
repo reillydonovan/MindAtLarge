a595899888091a90d9f92d4222d4851ffca6cb8f
Unknown <brandon.aleson@gmail.com>
2017-12-10 20:38:20 -0800

    adding light_scratch_pad scene and serialReadSwitchLight.cs script to demonstrate light switching functionality

---
+++ Assets/Scripts/serialReadSwitchLight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serialReadSwitchLight : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("COM3", 9600);
+	private Light nightLight;
+
+	void Start () {
+		stream.ReadTimeout = 50;  // ms
+		stream.Open();
+		nightLight = GetComponent<Light>();
+	}
+
+	void Update () {
+		try
+		{
+			string value = stream.ReadLine();
+			Debug.Log("received: " + value);
+			// string[] data = value.Split(',');
+			// return value;
+			int state = int.Parse(value);
+
+			if(state == 1)
+			{
+				nightLight.enabled = true;
+			}
+			else if(state == 0)
+			{
+				nightLight.enabled = false;
+			}
+		}
+		catch (TimeoutException)
+		{
+			// return null;
+		}
+	}
+}
