cb6081b2be5c5c981e5b405e76a7f76157325d0b
Ben Van Citters <van.citters.b@gmail.com>
2017-12-11 20:52:06 -0800

    tablecloth and screen

---
+++ Assets/Scripts/HandTableCloth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTableCloth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HandMethod(bool param)
+    {
+
+    }
+
+    public void HandNoParamMethod()
+    {
+
+    }
+
+
+
+    public bool boolMethod()
+    {
+        return false;
+    }
+}
