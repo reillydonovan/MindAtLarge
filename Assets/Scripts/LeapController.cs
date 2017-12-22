e23f3df2c4cbe0ecb74d95e88ee51381aa3d8d76
Ben V <van.citters.b@gmail.com>
2017-12-22 14:12:59 -0800

    leap experimet, new tablecloth material. moving script files. scene changes

---
+++ Assets/Scripts/LeapController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractionEngineUtility;
+using Leap.Unity.Attributes;
+using Leap.Unity;
+    using Leap.Unity.RuntimeGizmos;
+using Leap.Unity.Query;
+using Leap.Unity.Interaction.Internal;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LeapController : MonoBehaviour {
+
+    RenderTexture rt;
+    MeshRenderer mr;
+    LeapImageRetriever lr;
+    // Use this for initialization
+    void Start () {
+
+        mr = GetComponent<MeshRenderer>();
+        lr = GetComponent<LeapImageRetriever>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Material mat = mr.material;
+       
+        if (lr)
+        {
+            mat.mainTexture = lr.TextureData.RawTexture.CombinedTexture;
+        }
+    }
+}
