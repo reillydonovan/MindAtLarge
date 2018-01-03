05046a7461916750c971ccadabe882d1a8183fed
reillydonovan <reillydonovan3@gmail.com>
2018-01-03 15:43:36 -0800

    proximity effect getting close

--- Assets/Scripts/ProximityEffect.cs
+++ Assets/Scripts/ProximityEffect.cs
@@ -6,30 +6,24 @@ public class ProximityEffect : MonoBehaviour
 {
     public Transform target;
 
-    void Start()
-    {
+    void Update() { 
 
-        StartCoroutine(AdjustSaturation());
 
-    }
+            float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
-    IEnumerator AdjustSaturation()
-    {
+            if (distanceToTarget < 1) distanceToTarget = 1;
 
-        while (true)
-        {
+           // Remap(distanceToTarget, 0, 1, -1, 1);
+            Renderer rend = GetComponent<Renderer>();
+            rend.material.SetVector("_HSLAAdjust", new Vector4(0, 1 / distanceToTarget, 0, 0));
 
-           
-                float distanceToTarget = Vector3.Distance(transform.position, target.position); 
-
-                if (distanceToTarget < 1) distanceToTarget = 1;
-
-                Debug.Log(distanceToTarget);
-
-                yield return new WaitForSeconds(0.01f);
-
-            
-        }
+            Debug.Log(distanceToTarget);
 
     }
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+
 }
