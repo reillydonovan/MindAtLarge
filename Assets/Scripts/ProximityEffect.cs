27748eb40bffec418cdc66c853758386f7ddd772
reillydonovan <reillydonovan3@gmail.com>
2018-01-03 14:59:47 -0800

    started proximity effect script
    currently just debug logs proximity distance

---
+++ Assets/Scripts/ProximityEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityEffect : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+
+        StartCoroutine(AdjustSaturation());
+
+    }
+
+    IEnumerator AdjustSaturation()
+    {
+
+        while (true)
+        {
+
+           
+                float distanceToTarget = Vector3.Distance(transform.position, target.position); 
+
+                if (distanceToTarget < 1) distanceToTarget = 1;
+
+                Debug.Log(distanceToTarget);
+
+                yield return new WaitForSeconds(0.01f);
+
+            
+        }
+
+    }
+}
